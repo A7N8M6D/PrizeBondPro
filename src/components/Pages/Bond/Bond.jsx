c1c04ef2f041928a19446e990f9940a537d3c132
ANMD <nosha3173@gmail.com>
2024-05-26 05:31:25 +0500

    ssa

--- src/components/Pages/Bond/Bond.jsx
+++ src/components/Pages/Bond/Bond.jsx
@@ -7,12 +7,12 @@ const Bonds = () => {
     const navigate = useNavigate();
 
     return (
-        
+
         <div>
             <h2>Bonds Page</h2>
             <p>Welcome to the Bonds page!</p>
             <button onClick={() => navigate('/add-bond')}>
-                <FaMoneyCheckAlt /> Add New Bond
+                <FaMoneyCheckAlt /> Add New bonds
             </button>
         </div>
     );
